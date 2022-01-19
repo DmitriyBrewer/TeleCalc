00cc8621c1f209d9bda996967e17f5a8e0789696
Dmitriy <istrabrew@mail.ru>
2022-01-19 15:11:51 +0300

    testServer

--- Post.js
+++ Post.js
@@ -1,17 +1,29 @@
 import mongoose from "mongoose";
 
+// const Post = new mongoose.Schema({
+//   author: { type: String, required: "true" },
+//   title: { type: String, required: "true" },
+//   content: { type: String, required: "true" },
+//   picture: { type: String }
+// });
 const Post = new mongoose.Schema({
-  author: { type: String, required: "true" },
-  title: { type: String, required: "true" },
-  content: { type: String, required: "true" },
-  picture: { type: String }
+  author: { type: String },
+  title: { type: String },
+  content: { type: String },
+  picture: { type: String },
+  min: { type: String },
+  sms: { type: String },
+  gb: { type: String },
+  facebook: { type: String },
+  instagram: { type: String },
+  vk: { type: String },
+  pinterest: { type: String },
+  linkedin: { type: String },
+  whatsapp: { type: String },
+  telegram: { type: String },
+  message: { type: String },
+  mail: { type: String },
+  inbox: { type: String },
 });
 
 export default mongoose.model("Post", Post); //название 'Post' модель Post
-
-// {
-//   "author": "s",
-//    "title": "sd",
-//    "content": "2",
-//    "picture": "1"
-//  }
--- index.js
+++ index.js
@@ -11,7 +11,17 @@ const DB_URL =
 
 const app = express();
 
-app.use(express.json());
+
+//Cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  //Cors
+  app.use(express.json());
+
 
 app.use("/api", router);
 
