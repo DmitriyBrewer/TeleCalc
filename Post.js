b864656845c78d5a9df62438bf2cf7e1a744bcfe
Dmitriy <istrabrew@mail.ru>
2022-01-19 15:45:49 +0300

    other

--- Post.js
+++ Post.js
@@ -1,16 +1,6 @@
 import mongoose from "mongoose";
 
-// const Post = new mongoose.Schema({
-//   author: { type: String, required: "true" },
-//   title: { type: String, required: "true" },
-//   content: { type: String, required: "true" },
-//   picture: { type: String }
-// });
 const Post = new mongoose.Schema({
-  author: { type: String },
-  title: { type: String },
-  content: { type: String },
-  picture: { type: String },
   min: { type: String },
   sms: { type: String },
   gb: { type: String },
--- router.js
+++ router.js
@@ -1,19 +1,8 @@
 import Router from "express";
-
 import PostController from "./PostController.js";
 
 const router = new Router();
 
-//Раздел 5 пункт 4
-
-router.post("/posts", PostController.create); //Раздел 6 пункт 3
-
-router.get("/posts", PostController.getAll);
-
-router.get("/posts:id", PostController.getOne);
-
-router.put("/posts", PostController.update);
-
-router.delete("/posts:id", PostController.delete);
+router.post("/posts", PostController.create); 
 
 export default router;
